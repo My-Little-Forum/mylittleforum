0b472324442c79198ac6fe16234502951bbcaefa
Heiko August <post@auge8472.de>
2016-08-04 21:01:36 +0200

    Change: set the URL to the new download location

--- update/update_2.x-2.3.php
+++ update/update_2.x-2.3.php
@@ -17,7 +17,7 @@ if($_SESSION[$settings['session_prefix'].'user_type']!=2) exit;
 // update data:
 $update['version'] = array('2.0 RC 1','2.0 RC 2','2.0 RC 3','2.0 RC 4','2.0 RC 5','2.0 RC 6','2.0 RC 7','2.0 RC 8','2.0','2.0.1','2.0.2','2.1 beta 1','2.1 beta 2','2.1 beta 3','2.1 beta 4','2.1 beta 5','2.1 beta 6','2.1 beta 7','2.1 beta 8','2.1','2.1.1','2.1.2','2.1.3','2.1.4','2.2','2.2.1','2.2.2','2.2.3','2.2.4','2.2.5','2.2.6','2.2.7','2.2.8','2.3','2.3.1','2.3.2','2.3.3','2.3.4');
 $update['new_version'] = '2.3.5';
-$update['download_url'] = 'https://sourceforge.net/projects/mylittleforum/files/latest/download';
+$update['download_url'] = 'https://github.com/ilosuna/mylittleforum/releases/latest';
 #$update['message'] = '<p>HTML formated message...</p>';
 
 // changed files (folders followed by a slash like this: folder/):
