9bf2897d7c9f32ffff89544fd957222a52421c6a
Micha <GitHub@derletztekick.de>
2016-10-16 13:39:57 +0200

    Prevent 404-Error
    - Add path to favicon of the default template to prevent a 404 error

--- config/db_settings.php
+++ config/db_settings.php
@@ -1,27 +1,18 @@
 <?php
-// Database host, mostly 'localhost':
 $db_settings['host'] = 'localhost';
-
-// Database name:
-$db_settings['database'] = '';
-
-// Database user name:
-$db_settings['user'] = '';
-
-// Database password:
-$db_settings['password'] = '';
-
-// Database tables (normally not necessary to edit): 
-$db_settings['settings_table'] =       'mlf2_settings';
-$db_settings['forum_table'] =          'mlf2_entries';
-$db_settings['category_table'] =       'mlf2_categories';
-$db_settings['userdata_table'] =       'mlf2_userdata';
-$db_settings['smilies_table'] =        'mlf2_smilies';
-$db_settings['pages_table'] =          'mlf2_pages';
-$db_settings['banlists_table'] =       'mlf2_banlists';
-$db_settings['useronline_table'] =     'mlf2_useronline';
-$db_settings['login_control_table'] =  'mlf2_logincontrol';
-$db_settings['entry_cache_table'] =    'mlf2_entries_cache';
+$db_settings['user'] = 'root';
+$db_settings['password'] = 'root';
+$db_settings['database'] = 'mylittleforum';
+$db_settings['settings_table'] = 'mlf2_settings';
+$db_settings['forum_table'] = 'mlf2_entries';
+$db_settings['category_table'] = 'mlf2_categories';
+$db_settings['userdata_table'] = 'mlf2_userdata';
+$db_settings['smilies_table'] = 'mlf2_smilies';
+$db_settings['pages_table'] = 'mlf2_pages';
+$db_settings['banlists_table'] = 'mlf2_banlists';
+$db_settings['useronline_table'] = 'mlf2_useronline';
+$db_settings['login_control_table'] = 'mlf2_logincontrol';
+$db_settings['entry_cache_table'] = 'mlf2_entries_cache';
 $db_settings['userdata_cache_table'] = 'mlf2_userdata_cache';
-$db_settings['bookmark_table'] =       'mlf2_bookmarks';
+$db_settings['bookmark_table'] = 'mlf2_bookmarks';
 ?>
