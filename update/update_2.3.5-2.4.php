398fde544e87d3a35fb1b5158e52e810d990082e
Heiko August <post@auge8472.de>
2016-11-04 20:22:12 +0100

    Fix: stop update even with equal version numbers of current installation and update version

--- update/update_2.3.5-2.4.php
+++ update/update_2.3.5-2.4.php
@@ -131,7 +131,7 @@ if(!file_exists('../config/VERSION')) {
 }
 if (empty($update['errors'])) {
 	$newVersion = trim(file_get_contents('../config/VERSION'));
-	if ($newVersion < $settings['version']) {
+	if ($newVersion <= $settings['version']) {
 		$update['errors'][] = 'Error in line '.__LINE__.': The version you want to install (see string in config/VERSION) must be greater than the current installed version. Current version: '. htmlspecialchars($settings['version']) .', version you want to install: '.  htmlspecialchars($newVersion) .'.';
 	}
 	if(!in_array($settings['version'], $newVersion)) {
