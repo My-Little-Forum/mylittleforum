78474bcdd442661ca731227c9ebdf2f03f62bafc
Micha <GitHub@derletztekick.de>
2017-04-08 12:02:39 +0200

    check fields in array
    - add isset-condition to array items

--- includes/thread.inc.php
+++ includes/thread.inc.php
@@ -265,7 +265,7 @@ else $order = 'time';
 					$data['new'] = true;
 				} else {
 					$data['is_read'] = false;
-					if (isset($_SESSION[$settings['session_prefix'].'usersettings']['newtime']) && $_SESSION[$settings['session_prefix'].'usersettings']['newtime'] < $data['time'] || ($last_visit && ($data['last_reply'] > $last_visit or $data['time'] > $last_visit))) {
+					if (isset($_SESSION[$settings['session_prefix'].'usersettings']['newtime']) && $_SESSION[$settings['session_prefix'].'usersettings']['newtime'] < $data['time'] || ($last_visit && (isset($data['last_reply']) && $data['last_reply'] > $last_visit or isset($data['time']) && $data['time'] > $last_visit))) {
 						$data['new'] = true;
 					} else {
 						$data['new'] = false;
