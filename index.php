5f7b857dc9d03080a3ec1b9b7d846874a2c839d2
Heiko August <post@auge8472.de>
2017-07-09 21:48:11 +0200

    Change: add new version string to index.php and config/VERSION

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
  * @author Michael Lösler (https://github.com/derletztekick)
  * @author H. August (https://github.com/auge8472)
  * @copyright 2006-2017 Mark Alexander Hoschek
- * @version 2.4.2 (2017-03-12)
+ * @version 2.4.3 (2017-07-09)
  * @link http://mylittleforum.net/
  *
  * This program is free software: you can redistribute it and/or modify
