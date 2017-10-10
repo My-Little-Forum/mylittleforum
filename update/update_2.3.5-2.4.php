78bd0967c73dfe095658df87dfa7a160fd719191
Micha <GitHub@derletztekick.de>
2017-10-10 20:10:45 +0200

    Add version
    - version number is added to update-array
    - https://github.com/ilosuna/mylittleforum/issues/264

--- update/update_2.3.5-2.4.php
+++ update/update_2.3.5-2.4.php
@@ -375,6 +375,10 @@ if(empty($update['errors'])) {
 	if(!@mysqli_query($connid, "UPDATE ".$db_settings['settings_table']." SET value='". mysqli_real_escape_string($connid, $newVersion) ."' WHERE name = 'version'")) {
 		$update['errors'][] = 'Database error in line '.__LINE__.': ' . mysqli_error($connid);
 	}
+	else {
+		// Set new Version, taken ftrom VERSION file.
+		$update['new_version'] = $newVersion;
+	}
 }
 
 ?>
