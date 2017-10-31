2c5b6dc54c46e5eefed854715165532af0774a1e
Heiko August <post@auge8472.de>
2017-10-31 19:13:54 +0100

    Cleanup: reformatting of account_locked.inc.php

--- includes/account_locked.inc.php
+++ includes/account_locked.inc.php
@@ -1,12 +1,11 @@
 <?php
-if(!defined('IN_INDEX'))
- {
-  header('Location: ../index.php');
-  exit;
- }
+if (!defined('IN_INDEX')) {
+	header('Location: ../index.php');
+	exit;
+}
 
-$smarty->assign('message','user_locked_message');
-$smarty->assign('subnav_location','subnav_locked');
-$smarty->assign('subtemplate','info.inc.tpl');
+$smarty->assign('message', 'user_locked_message');
+$smarty->assign('subnav_location', 'subnav_locked');
+$smarty->assign('subtemplate', 'info.inc.tpl');
 $template = 'main.tpl';
 ?>
