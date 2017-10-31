e57ec47dfe9eb9fb019dbd597b788aa5890d52d3
Heiko August <post@auge8472.de>
2017-10-31 19:01:25 +0100

    Cleanup: reformatting of delete_cookie.inc.php

--- includes/delete_cookie.inc.php
+++ includes/delete_cookie.inc.php
@@ -1,19 +1,18 @@
 <?php
-if(!defined('IN_INDEX'))
- {
-  header('Location: ../index.php');
-  exit;
- }
+if (!defined('IN_INDEX')) {
+	header('Location: ../index.php');
+	exit;
+}
 
-setcookie($settings['session_prefix'].'userdata','',0);
+setcookie($settings['session_prefix'].'userdata', '', 0);
 
-if(isset($_POST['method']) && $_POST['method']=='ajax') exit;
+if (isset($_POST['method']) && $_POST['method'] == 'ajax') exit;
 
-else
- {
-  $smarty->assign('message','cookie_deleted');
-  $smarty->assign('subnav_location','subnav_delete_cookie');
-  $smarty->assign('subtemplate','info.inc.tpl');
-  $template = 'main.tpl';
- }
+else {
+	$smarty->assign('message', 'cookie_deleted');
+	$smarty->assign('subnav_location', 'subnav_delete_cookie');
+	$smarty->assign('subtemplate', 'info.inc.tpl');
+	$template = 'main.tpl';
+}
+
 ?>
