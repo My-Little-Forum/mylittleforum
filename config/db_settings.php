3657badad54c0a7763b735d173a12c9553593a44
Micha <GitHub@derletztekick.de>
2017-11-28 20:35:52 +0100

    Add tables
    - tags_table for tag values
    - bookmark_tags_table for connection between tags and bookmarks

--- config/db_settings.php
+++ config/db_settings.php
@@ -12,18 +12,20 @@ $db_settings['user'] = '';
 $db_settings['password'] = '';
 
 // Database tables (normally not necessary to edit): 
-$db_settings['settings_table'] =       'mlf2_settings';
-$db_settings['forum_table'] =          'mlf2_entries';
-$db_settings['category_table'] =       'mlf2_categories';
-$db_settings['userdata_table'] =       'mlf2_userdata';
-$db_settings['smilies_table'] =        'mlf2_smilies';
-$db_settings['pages_table'] =          'mlf2_pages';
-$db_settings['banlists_table'] =       'mlf2_banlists';
-$db_settings['useronline_table'] =     'mlf2_useronline';
-$db_settings['login_control_table'] =  'mlf2_logincontrol';
-$db_settings['entry_cache_table'] =    'mlf2_entries_cache';
+$db_settings['settings_table']       = 'mlf2_settings';
+$db_settings['forum_table']          = 'mlf2_entries';
+$db_settings['category_table']       = 'mlf2_categories';
+$db_settings['userdata_table']       = 'mlf2_userdata';
+$db_settings['smilies_table']        = 'mlf2_smilies';
+$db_settings['pages_table']          = 'mlf2_pages';
+$db_settings['banlists_table']       = 'mlf2_banlists';
+$db_settings['useronline_table']     = 'mlf2_useronline';
+$db_settings['login_control_table']  = 'mlf2_logincontrol';
+$db_settings['entry_cache_table']    = 'mlf2_entries_cache';
 $db_settings['userdata_cache_table'] = 'mlf2_userdata_cache';
-$db_settings['bookmark_table'] =       'mlf2_bookmarks';
-$db_settings['read_status_table'] =    'mlf2_read_entries';
-$db_settings['temp_infos_table'] =     'mlf2_temp_infos';
+$db_settings['bookmark_table']       = 'mlf2_bookmarks';
+$db_settings['read_status_table']    = 'mlf2_read_entries';
+$db_settings['temp_infos_table']     = 'mlf2_temp_infos';
+$db_settings['tags_table']           = 'mlf2_tags';
+$db_settings['bookmark_tags_table']  = 'mlf2_bookmark_tags';
 ?>
