a066ed5563aae608a73cd162a3c38fb2ea6cb01c
Heiko August <post@auge8472.de>
2019-01-06 13:31:32 +0100

    Change: new version number string (2.4.17) in index.php and config/VERSION

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
  * @author Michael Lösler (https://github.com/loesler)
  * @author Heiko August (https://github.com/auge8472)
  * @copyright 2006-2018 Mark Alexander Hoschek
- * @version 2.4.16 (2018-12-07)
+ * @version 2.4.17 (2019-01-06)
  * @link https://mylittleforum.net/
  *
  * This program is free software: you can redistribute it and/or modify
