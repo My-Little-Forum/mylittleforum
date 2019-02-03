6fe47340bf47833718cbb8acc01d61e9dc50e02d
Heiko August <post@auge8472.de>
2019-02-03 16:57:40 +0100

    Change: new version number 2.4.19

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
  * @author Michael Lösler (https://github.com/loesler)
  * @author Heiko August (https://github.com/auge8472)
  * @copyright 2006-2018 Mark Alexander Hoschek
- * @version 2.4.18.1 (2019-01-14)
+ * @version 2.4.19 (2019-02-03)
  * @link https://mylittleforum.net/
  *
  * This program is free software: you can redistribute it and/or modify
