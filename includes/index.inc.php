44c550200353ff8e23a65f5dd3e815cc94d6308a
Heiko August <post@auge8472.de>
2019-05-14 22:05:02 +0200

    Cleanup: remove trailing tabs from the end of a code line

--- includes/index.inc.php
+++ includes/index.inc.php
@@ -80,7 +80,7 @@ if ($result_count > 0) {
 		$thread_result = @mysqli_query($connid, "SELECT id, pid, tid, ft.user_id, user_type, UNIX_TIMESTAMP(ft.time) AS time, UNIX_TIMESTAMP(ft.time + INTERVAL ".intval($time_difference)." MINUTE) AS timestamp, UNIX_TIMESTAMP(last_reply) AS last_reply, 
 		    name, user_name, subject, IF(text='',true,false) AS no_text, category, views, marked, locked, sticky, rst.user_id AS req_user,
 			" . $db_settings['akismet_rating_table'] . ".spam AS akismet_spam,
-			" . $db_settings['b8_rating_table'] . ".spam AS b8_spam				
+			" . $db_settings['b8_rating_table'] . ".spam AS b8_spam
 			FROM ".$db_settings['forum_table']." AS ft
 			LEFT JOIN ".$db_settings['userdata_table']." ON ".$db_settings['userdata_table'].".user_id = ft.user_id
 			LEFT JOIN ".$db_settings['read_status_table']." AS rst ON rst.posting_id = ft.id AND rst.user_id = ". intval($tmp_user_id) ."
