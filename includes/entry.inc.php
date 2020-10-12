a8d9aeab88f5a85817c74b3ad217c35b22f6c907
Heiko August <post@auge8472.de>
2020-10-12 11:55:19 +0200

    Change: add field user_type to the thread tree informations and add classes to the user name elements

--- includes/entry.inc.php
+++ includes/entry.inc.php
@@ -134,7 +134,7 @@
 	if ($entrydata['spam'] == 1)
 		$display_spam_query_and = '';
 	$result = mysqli_query($connid, "SELECT id, pid, tid, ft.user_id, UNIX_TIMESTAMP(ft.time) AS time, UNIX_TIMESTAMP(ft.time + INTERVAL " . $time_difference . " MINUTE) AS disp_time,
-                        UNIX_TIMESTAMP(last_reply) AS last_reply, name, user_name, subject, category, marked, text, spam, rst.user_id AS req_user FROM " . $db_settings['forum_table'] . " AS ft
+                        UNIX_TIMESTAMP(last_reply) AS last_reply, name, user_name, user_type, subject, category, marked, text, spam, rst.user_id AS req_user FROM " . $db_settings['forum_table'] . " AS ft
                         LEFT JOIN " . $db_settings['userdata_table'] . " ON " . $db_settings['userdata_table'] . ".user_id=ft.user_id
                         LEFT JOIN " . $db_settings['read_status_table'] . " AS rst ON rst.posting_id = ft.id AND rst.user_id = " . intval($tmp_user_id) . "
                         WHERE tid = " . $thread . $display_spam_query_and . " ORDER BY time ASC");
