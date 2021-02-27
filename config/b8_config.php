7bc34e051e5eb452174e8965bdc09f6e60e5ea13
Michael Loesler <loesler@applied-geodesy.org>
2021-02-27 15:09:18 +0100

    Update module
    - update B8 version 0.7
    - namespace is added (e.g. b8::HAM --> b8\b8::HAM)

--- config/b8_config.php
+++ config/b8_config.php
@@ -9,7 +9,6 @@ $B8_CONFIG_LEXER = array(
 	'min_size'      => 3,
 	'max_size'      => 30,
 	'allow_numbers' => FALSE,
-	'old_get_html'  => FALSE,
 	'get_html'      => TRUE,
 	'get_uris'      => TRUE,
 	'get_bbcode'    => FALSE
@@ -20,16 +19,13 @@ $B8_CONFIG_DEGENERATOR = array(
 	'multibyte' => function_exists('mb_strtolower') && function_exists('mb_strtoupper') && function_exists('mb_substr')
 );
 
-$B8_CONFIG_DATABASE = array(
-	'storage' => 'mysqli'
+$B8_CONFIG_DATABASE_TYPE = array(
+	'storage' => 'mysql'
 );
 
-$B8_CONFIG_AUTHENTICATION = array(
-	'database'   => $db_settings['database'],
-	'table_name' => $db_settings['b8_wordlist_table'],
-	'host'       => $db_settings['host'],
-	'user'       => $db_settings['user'],
-	'pass'       => $db_settings['password']
+$B8_CONFIG_STORAGE = array(
+	'resource' => new mysqli($db_settings['host'], $db_settings['user'], $db_settings['password'], $db_settings['database']),
+	'table'    => $db_settings['b8_wordlist_table']
 );
 /** config for b8 filter **/
 ?>
