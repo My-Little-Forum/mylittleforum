e10bcd8b7552c0d3915da674e764b733d7600559
Michael Loesler <loesler@applied-geodesy.org>
2021-02-28 21:22:04 +0100

    Update b8.php
    - modify spl_autoload_register function to prevent errors
    - cf. https://gitlab.com/l3u/b8/-/merge_requests/1

--- modules/b8/b8.php
+++ modules/b8/b8.php
@@ -31,9 +31,11 @@ namespace b8;
 
 spl_autoload_register(
     function ($class) {
-        $parts = explode('\\', $class);
-        require_once __DIR__ . DIRECTORY_SEPARATOR . $parts[1]
+		$parts = explode('\\', $class);
+		if (count($parts) > 2 && $parts[0] == 'b8') {
+			require_once __DIR__ . DIRECTORY_SEPARATOR . $parts[1]
                      . DIRECTORY_SEPARATOR . $parts[2] . '.php';
+		}
     }
 );
 
