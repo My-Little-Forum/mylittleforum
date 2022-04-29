13924604fde924196530f29902de6412816403f0
Michael Lösler <loesler@applied-geodesy.org>
2022-04-29 16:10:47 +0200

    get_magic_quotes_gpc removed (#590)
    - get_magic_quotes_gpc removed because this function returns alwas false
    - get_magic_quotes_gpc is deprecated cf. https://www.php.net/manual/en/function.get-magic-quotes-gpc.php
    - see also #552, #553

--- install/index.php
+++ install/index.php
@@ -28,18 +28,6 @@ $default_settings['forum_name'] = 'my little forum';
 $default_settings['forum_address'] = ((isProtocolHTTPS() === true) ? 'https' : 'http') .'://'. $_SERVER['HTTP_HOST'] . substr(rtrim(dirname($_SERVER['SCRIPT_NAME']), '/\\'), 0, strrpos(rtrim(dirname($_SERVER['SCRIPT_NAME']), '/\\'), '/')) . '/';
 $default_settings['table_prefix'] = 'mlf2_';
 
-// stripslashes on GPC if get_magic_quotes_gpc is enabled:
-if (get_magic_quotes_gpc()) {
-	function stripslashes_deep($value) {
-		$value = is_array($value) ? array_map('stripslashes_deep', $value) : stripslashes($value);
-		return $value;
-	}
-	$_POST = array_map('stripslashes_deep', $_POST);
-	$_GET = array_map('stripslashes_deep', $_GET);
-	$_COOKIE = array_map('stripslashes_deep', $_COOKIE);
-	$_REQUEST = array_map('stripslashes_deep', $_REQUEST);
-}
-
 function table_exists($table) {
 	global $connid;
 	$result = @mysqli_query($connid, "SHOW TABLES");
