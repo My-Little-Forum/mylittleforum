b327f6880cb45eb099442577d5f42e2b282bf063
Heiko August <post@auge8472.de>
2023-02-13 22:53:30 +0100

    Change: reformat modules/captcha/captcha_image.php

--- modules/captcha/captcha_image.php
+++ modules/captcha/captcha_image.php
@@ -1,14 +1,11 @@
 <?php
-ini_set('error_reporting','E_ALL');
+ini_set('error_reporting', 'E_ALL');
 session_start();
 require('captcha.php');
 $captcha = new Captcha();
-if(isset($_SESSION['captcha_session']))
- {
-  $captcha->generate_image($_SESSION['captcha_session'],'backgrounds/','fonts/');
- }
-else
- {
-  $captcha->generate_dummy_image();
- }
+if (isset($_SESSION['captcha_session'])) {
+	$captcha->generate_image($_SESSION['captcha_session'], 'backgrounds/', 'fonts/');
+} else {
+	$captcha->generate_dummy_image();
+}
 ?>
