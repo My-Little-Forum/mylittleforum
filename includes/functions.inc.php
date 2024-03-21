84c3cbbbe6fea5c9f30ad64148003dd7bb6649ad
Heiko August <post@auge8472.de>
2024-03-21 22:02:04 +0100

    Change: reformatting of the function do_bbcode_msg

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -518,22 +518,17 @@ function do_bbcode_url ($action, $attributes, $content, $params, $node_object) {
 /**
  * processes BBCode message links
  */
-function do_bbcode_msg($action, $attributes, $content, $params, $node_object)
- {
-  if ($action == 'validate')
-   {
-    if(!isset($attributes['default']))
-     {
-      if(intval($content)>0) return true;
-     }
-    if(intval($attributes['default'])>0) return true;
-   }
-  else
-   {
-    if(!isset ($attributes['default'])) return '<a href="index.php?id='.intval($content).'" class="internal">'.intval($content).'</a>';
-    return '<a href="index.php?id='.intval($attributes['default']).'" class="internal">'.$content.'</a>';
-   }
- }
+function do_bbcode_msg($action, $attributes, $content, $params, $node_object) {
+	if ($action == 'validate') {
+		if (!isset($attributes['default'])) {
+			if (intval($content) > 0) return true;
+		}
+		if (intval($attributes['default']) > 0) return true;
+	} else {
+		if (!isset($attributes['default'])) return '<a href="index.php?id='.intval($content).'" class="internal">'.intval($content).'</a>';
+		return '<a href="index.php?id='.intval($attributes['default']).'" class="internal">'.$content.'</a>';
+	}
+}
 
 /**
  * processes BBCode img
