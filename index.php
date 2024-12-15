7c9824c5eb43d98c334ab8bb1f79f3518c7c3bf3
Heiko August <post@auge8472.de>
2024-12-15 22:47:10 +0100

    Change: add the new version number to the files index.php and config/VERSION

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
  * @author Michael Lösler (https://github.com/loesler)
  * @author Heiko August (https://github.com/auge8472)
  * @copyright 2006-2024 Mark Alexander Hoschek
- * @version 20240827.1 (2024-08-27)
+ * @version 20241215.1 (2024-12-15)
  * @link https://mylittleforum.net/
  *
  * This program is free software: you can redistribute it and/or modify
