c24ff4e7a0eb56a2979de2478898bd57d8497012
Michael Lösler <loesler@applied-geodesy.org>
2025-05-25 22:20:24 +0200

    BB-code media (#827)
    Inspired by #822 and w.r.t. the discussion given in #628 the bb-code media is added to the user-interface, i.e. [media=audio]https://example.org/audio.mp3[/media] or [media=video]https://example.org/video.mp4[/media]. URLs are not converted automatically

--- includes/js_defaults.inc.php
+++ includes/js_defaults.inc.php
@@ -56,6 +56,9 @@ lang["hide_password_title"] =              "<?php echo addslashes($lang['hide_pa
 		lang["bbcode_link_url"] =                  "<?php echo addslashes($lang['bbcode_link_url']); ?>";
 		lang["bbcode_image_url"] =                 "<?php echo addslashes($lang['bbcode_image_url']); ?>";
 	<?php endif; ?>
+	<?php if($settings['bbcode_media']): ?>
+		lang["bbcode_media_url"] =                 "<?php echo addslashes($lang['bbcode_media_url']); ?>";
+	<?php endif; ?>
 	<?php if($settings['bbcode_latex'] && !empty($settings['bbcode_latex_uri'])): ?>
 		lang["bbcode_tex_code"] =                  "<?php echo addslashes($lang['bbcode_tex_code']); ?>";
 	<?php endif; ?>
