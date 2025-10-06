c3cb0b6971b754bbef50223f95f432181478c61f
Heiko August <post@auge8472.de>
2025-10-06 16:42:35 +0200

    Cleanup: remove commented code, the function gets called in the index.php

--- includes/main.inc.php
+++ includes/main.inc.php
@@ -78,9 +78,6 @@ if (function_exists('date_default_timezone_set')) {
 	}
 }
 
-// do daily actions:
-//daily_actions(TIMESTAMP);
-
 $categories = get_categories();
 $category_ids = get_category_ids($categories);
 if ($category_ids != false) $category_ids_query = implode(', ', $category_ids);
