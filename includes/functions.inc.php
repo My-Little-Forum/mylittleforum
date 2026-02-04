95cd0df17cd2dc7b6cdf7c553cd3778a6f00a987
Heiko August <post@auge8472.de>
2026-02-04 13:30:31 +0100

    Fix: change domain name to youtube-nocookie.com in Youtube iframe to prevent error 153 …
    … as advised in https://appuals.com/youtube-error-code-153-player-configuration-error, point 5
    
    Signed-off-by: Heiko August <post@auge8472.de>

--- includes/functions.inc.php
+++ includes/functions.inc.php
@@ -665,7 +665,7 @@ function do_bbcode_media($action, $attributes, $content, $params, $node_object) 
 				if (!empty($videoMatches) && isset($mediaTypes[$videoMatches[1]]))
 					$media = "<video width=\"" . $videoWidth . "\" height=\"" . $videoHeight . "\" controls src=\"" . htmlspecialchars($videoMatches[0]) . "\" type=\"video/" . htmlspecialchars($mediaTypes[$videoMatches[1]]) . "\">Your browser does not support the video tag. Please visit <a href=\"" . htmlspecialchars($videoMatches[0]) . "\">" . htmlspecialchars($videoMatches[0]) . "</a>.</video>";
 				else
-					$media = "<iframe width=\"" . $videoWidth . "\" height=\"" . $videoHeight . "\" src=\"https://www.youtube.com/embed/" . htmlspecialchars($youtubeMatches[1]) . "\" frameborder=\"0\" referrerpolicy=\"strict-origin-when-cross-origin\" allowfullscreen>Your browser does not support iframes. Please visit <a href=\"" . htmlspecialchars($youtubeMatches[0]) . "\">" . htmlspecialchars($youtubeMatches[0]) . "</a>.</iframe>";
+					$media = "<iframe width=\"" . $videoWidth . "\" height=\"" . $videoHeight . "\" src=\"https://www.youtube-nocookie.com/embed/" . htmlspecialchars($youtubeMatches[1]) . "\" frameborder=\"0\" referrerpolicy=\"strict-origin-when-cross-origin\" allowfullscreen>Your browser does not support iframes. Please visit <a href=\"" . htmlspecialchars($youtubeMatches[0]) . "\">" . htmlspecialchars($youtubeMatches[0]) . "</a>.</iframe>";
 			break;
 		}
 		return $media;
